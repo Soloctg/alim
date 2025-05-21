7c7a4cba115936452633c2d78c429bd9e0b056c0
solomon-ctg <solomon.iroegbu@gmail.com>
2025-05-21 12:15:29 +0100

    Feat: add white color to Details

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -45,15 +45,28 @@ class DetailsScreen extends StatelessWidget {
                   product.name,
                   style: const TextStyle(
                     fontSize: 22,
+                    color: Colors.white,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
                 const SizedBox(height: 8),
-                Text(product.description),
+                Text(
+                  product.description,
+                  style: const TextStyle(fontSize: 16, color: Colors.white),
+                ),
                 const SizedBox(height: 16),
+                const Text(
+                  "Price",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+
                 Text(
                   "\$${product.price.toStringAsFixed(2)}",
-                  style: const TextStyle(fontSize: 20),
+                  style: const TextStyle(fontSize: 20, color: Colors.white),
                 ),
                 const SizedBox(height: 20),
                 ElevatedButton(
